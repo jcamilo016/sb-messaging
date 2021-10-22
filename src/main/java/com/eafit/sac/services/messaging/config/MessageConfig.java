b4515d2b5c3783412d49c157fd2e09ab68d15e57
Juan Camilo Acosta <jcamilo016@gmail.com>
2021-10-22 18:12:59 -0500

    cross origin to allow any direction added

--- src/main/java/com/eafit/sac/services/messaging/config/MessageConfig.java
+++ src/main/java/com/eafit/sac/services/messaging/config/MessageConfig.java
@@ -1,9 +1,15 @@
 package com.eafit.sac.services.messaging.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -21,6 +27,12 @@ public class MessageConfig {
 
     @Value("${mailsender.username}")
     private String mailSenderUser;
+
+    @Value("${server.port.http}")
+    int httpPort;
+
+    @Value("${server.port}")
+    int httpsPort;
 
     @Autowired
     private EmailConfig emailConfig;
@@ -49,5 +61,31 @@ public class MessageConfig {
         props.put("mail.debug", "true");
 
         return mailSender;
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
     }
 }
